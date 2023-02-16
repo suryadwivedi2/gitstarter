09315cfa9878ef49a314b6e8117aed035aacd20c
suryadwivedi2 <bcae2.08924402018@gmail.com>
2023-02-16 19:06:58 +0530

    domcommit

---
+++ dom_task_3.js
@@ -0,0 +1,4 @@
+const headertitle=document.getElementById("main-header");
+headertitle.style.border='solid 3px black';
+const text=document.getElementById('header-title');
+text.style.color='green';
